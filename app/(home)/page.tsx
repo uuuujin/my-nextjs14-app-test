c70bd235a8bc6580f5dbfb23d67dcb730a4604c9
shin-yujin-mac <in1006sh@gmail.com>
2024-08-24 17:47:49 +0900

    Feature : learning nextjs Loading Components

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,31 +1,26 @@
-"use client"
-
-import {useEffect, useState} from "react";
 import {Metadata} from "next";
 
-//export const metadata:Metadata = {
-  //title: 'Home',
-  // description: 'The best Movies on the best framework',
-//}
+export const metadata:Metadata = {
+  title: 'Home',
+}
 // client 컴포넌트에서는 metadata export 할수 없음
 // rootlayout가 export default되어 있고, metadata는 export만 되고 있음
 // 당연 하게도 default export 객체는 하나만 지정 할수 있기 때문
 
-export default function Page() {
-    const [isLoading, setIsLoading] = useState(true);
-    const [movies, setMovies] = useState([]);
-    const getMovies = async () => {
-        const res = await fetch('https://nomad-movies.nomadcoders.workers.dev/movies');
-        const json = await res.json();
-        setMovies(json);
-        setIsLoading(false);
-    }
-    useEffect(() => {
-        getMovies();
-    }, []);
+const URL = 'https://nomad-movies.nomadcoders.workers.dev/movies';
+
+async function getMovies() {
+    await new Promise((resolve) => setTimeout(resolve, 10000));
+    const res = await fetch(URL);
+    const json = await res.json();
+    return json;
+}
+
+export default async function HomePage() {
+    const movies = await getMovies();
     return (
         <div>
-            {isLoading ? 'loading...': JSON.stringify(movies)}
+            { JSON.stringify(movies)}
         </div>
     )
 }
