bf3ea091c757631fc8c5b436cbb1416d96ce1078
shin-yujin-mac <in1006sh@gmail.com>
2024-09-28 16:23:17 +0900

    Feature : learning nextjs Suspense

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -5,10 +5,10 @@ export const metadata:Metadata = {
   title: 'Home',
 }
 
-export const API_URL = 'https://nomad-movies.nomadcoders.workers.dev/movies';
+export const API_URL:URL = 'https://nomad-movies.nomadcoders.workers.dev/movies';
 
 async function getMovies() {
-    await new Promise((resolve) => setTimeout(resolve, 10000));
+    await new Promise((resolve) => setTimeout(resolve, 1000));
     const res = await fetch(API_URL);
     const json = await res.json();
     return json;
