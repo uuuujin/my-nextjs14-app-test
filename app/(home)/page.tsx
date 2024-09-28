adceb80c04ca88f01b4b9bdcfd815f4a4ba932b4
shin-yujin-mac <in1006sh@gmail.com>
2024-09-28 15:46:54 +0900

    Feature : learning nextjs Parallel Requests (#3.4)

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,17 +1,15 @@
 import {Metadata} from "next";
+import Link from "next/link";
 
 export const metadata:Metadata = {
   title: 'Home',
 }
-// client 컴포넌트에서는 metadata export 할수 없음
-// rootlayout가 export default되어 있고, metadata는 export만 되고 있음
-// 당연 하게도 default export 객체는 하나만 지정 할수 있기 때문
 
-const URL = 'https://nomad-movies.nomadcoders.workers.dev/movies';
+export const API_URL = 'https://nomad-movies.nomadcoders.workers.dev/movies';
 
 async function getMovies() {
     await new Promise((resolve) => setTimeout(resolve, 10000));
-    const res = await fetch(URL);
+    const res = await fetch(API_URL);
     const json = await res.json();
     return json;
 }
@@ -20,7 +18,14 @@ export default async function HomePage() {
     const movies = await getMovies();
     return (
         <div>
-            { JSON.stringify(movies)}
+            {movies.map(movie =>
+                <li>
+                    <Link href={`/movies/${movie.id}`}>
+                        {movie.title}
+                    </Link>
+                </li>
+                )
+            }
         </div>
     )
 }
--- app/(movies)/movies/[id]/page.tsx
+++ app/(movies)/movies/[id]/page.tsx
@@ -1,5 +1,29 @@
-export default function MovieDetail({params: {id}} : {params: {id: string}}) {
-    return <h1>Movies {id}</h1>;
+import {API_URL} from "../../../(home)/page";
+
+async function getMovie(id : string) {
+    console.log(`fetching movies: ${Date.now()}`);
+    await new Promise((resolve) => setTimeout(resolve, 500));
+    const response = await fetch(`${API_URL}/${id}`);
+    return response.json();
+}
+
+async function getVideos(id: string) {
+    console.log(`fetching video: ${Date.now()}`);
+    await new Promise((resolve) => setTimeout(resolve, 500));
+    const response = await fetch(`${API_URL}/${id}/videos`);
+    return response.json();
+}
+
+export default async function MovieDetail({
+    params: {id}
+} : {
+    params: { id: string }
+}) {
+    console.log('start fetching')
+    const [movie, videos] = await Promise.all([getMovie(id), getVideos(id)]);
+    console.log('end fetching')
+
+    return <h1>{movie.title} {id}</h1>;
 }
 
 // /movies/:id
